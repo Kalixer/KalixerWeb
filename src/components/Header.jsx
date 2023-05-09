4de66173d5c000119a893a96981dc879a39660c3
Kalixer <al.caceres.ag72@gmail.com>
2023-05-09 15:46:18 -0400

    [Upgrade] now the 'contact' icon changes it's color when you place the mouse over it and comes back to white once you leave it

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import '@styles/Header.scss';
 
-const Header = () => {
+const contact_white = <img src="https://img.icons8.com/small/32/FFFFFF/user.png"/>
+const contact_taupe = <img src="https://img.icons8.com/small/32/988780/user.png"/>
 
-    const contact = <img src="https://img.icons8.com/small/32/FFFFFF/user.png"/>
+const Header = () => {
+    let overContact = false; // This is for when you place the mouse over the contact icon
     
+    const [colorIcon, setcolorIcon] = useState(contact_white)
+
+    const setTaupe = () => {
+        setcolorIcon(contact_taupe)
+    }
+    const setWhite = () => {
+        setcolorIcon(contact_white)
+    }
+
     return (
         <div className="header">
             <button className="menu-responsive">
@@ -23,8 +34,8 @@ const Header = () => {
                 <li>Contact</li>
             </ul>
 
-            <div className="contact-responsive">
-                {contact}
+            <div className="contact-responsive" onMouseOver={setTaupe} onMouseLeave={setWhite}>
+                {colorIcon}
             </div>
         </div>
     );
