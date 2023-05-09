dee2cab83a782de649898b6b4e73a5ecfac766c8
Kalixer <al.caceres.ag72@gmail.com>
2023-05-09 15:30:50 -0400

    [Responsive Add] Button with animation added. The button doesn't work yet, it only has an animation. An icon on the right side was added too

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,8 +2,16 @@ import React from "react";
 import '@styles/Header.scss';
 
 const Header = () => {
+
+    const contact = <img src="https://img.icons8.com/small/32/FFFFFF/user.png"/>
+    
     return (
         <div className="header">
+            <button className="menu-responsive">
+                <div></div>
+                <div></div>
+                <div></div>
+            </button>
             <div className="name">
                 <p>Kalixer</p>
             </div>
@@ -14,6 +22,10 @@ const Header = () => {
                 <li>Portfolio</li>
                 <li>Contact</li>
             </ul>
+
+            <div className="contact-responsive">
+                {contact}
+            </div>
         </div>
     );
 }
