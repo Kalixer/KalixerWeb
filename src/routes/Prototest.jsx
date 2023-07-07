c23f01ee19dc878d12284d9777d431679df9c691
kalixer <al.caceres.ag72@gmail.com>
2023-07-07 15:17:54 -0400

    [Add] I created a .jsx and a .scss files named 'prototest' to use as a tester for the header buttons that should lead to other pages

---
+++ src/routes/Prototest.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import "@styles/Prototest.scss"
+const Prototest = () => {
+    return (
+        <div className="prototest">
+            <h1 className='title'>Hello prototest</h1>
+        </div>
+    )
+}
+
+export default Prototest;
