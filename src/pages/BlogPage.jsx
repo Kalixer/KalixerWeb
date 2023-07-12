d6453b6ed3432a4a73351617b0541ea3aaf3d98a
kalixer <al.caceres.ag72@gmail.com>
2023-07-12 17:20:39 -0400

    [Progress] Now there are a section with information about the page (lorem impsum) and a prototype of blog entries. It's like the mockup

--- src/pages/BlogPage.jsx
+++ src/pages/BlogPage.jsx
@@ -5,6 +5,16 @@ import '@styles/BlogPage.scss';
 
 const blogDescription = `Unraveling the labyrinth of an Introverts mind`
 
+const introExp1 = `Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laboriosam optio molestias fugiat temporibus laborum, distinctio, est error, sit deserunt libero fugit porro cum reiciendis ducimus numquam accusamus. Dicta, quas similique.`
+
+const introExp2 = `Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laboriosam optio molestias fugiat temporibus laborum, distinctio, est error, sit deserunt libero fugit porro cum reiciendis ducimus numquam accusamus. Dicta, quas similique.`
+
+const introExp3 = `Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laboriosam optio molestias fugiat temporibus laborum, distinctio, est error, sit deserunt libero fugit porro cum reiciendis ducimus numquam accusamus. Dicta, quas similique.`
+
+const introExp4 = `Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laboriosam optio molestias fugiat temporibus laborum, distinctio, est error, sit deserunt libero fugit porro cum reiciendis ducimus numquam accusamus. Dicta, quas similique.`
+
+const introExp5 = `Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laboriosam optio molestias fugiat temporibus laborum, distinctio, est error, sit deserunt libero fugit porro cum reiciendis ducimus numquam accusamus. Dicta, quas similique.`
+
 const BlogPage = () => {
     return (
         <div className='BlogPage'>
@@ -14,7 +24,40 @@ const BlogPage = () => {
                 <p>{blogDescription}</p>
                 <p>Or at least trying to</p>
             </div>
-            <section className='content'></section>
+            <section className='content'>
+                <div className='intro'>
+                    <p className='p-intro'>
+                        {introExp1}
+                    </p>
+                    <p className='p-intro'>
+                        {introExp2}
+                    </p>
+                    <p className='p-intro'>
+                        {introExp3}
+                    </p>
+                    <p className='p-intro'>
+                        {introExp4}
+                    </p>
+                    <p className='p-intro'>
+                        {introExp5}
+                    </p>
+                </div>
+                <div className='entries-section'>
+                    <h1>Entries</h1>
+                    <div className='entries'>
+                        <div className='entrie-by-three'>
+                            <div className='entrie'></div>
+                            <div className='entrie'></div>
+                            <div className='entrie'></div>
+                        </div>
+                        <div className='entrie-by-three'>
+                            <div className='entrie'></div>
+                            <div className='entrie'></div>
+                            <div className='entrie'></div>
+                        </div>
+                    </div>
+                </div>
+            </section>
         </div>
     )   
 }
