57274401d11886339a30668d19fd912d20c6f4c4
kalixer <al.caceres.ag72@gmail.com>
2023-07-13 10:19:28 -0400

    [Progress] I 'finished' the concept of the entry blogs grid. I still want to make some more changes though

--- src/components/Entry.jsx
+++ src/components/Entry.jsx
@@ -2,14 +2,21 @@ import React from 'react';
 
 import '@styles/Entry.scss';
 
+
+const imagenTest = `https://www.research4life.org/wp-content/uploads/2020/05/AdobeStock_162765779_2x1.jpg`
+const loremTest = `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae exercitationem ipsa illo vero hic reiciendis nemo eaque adipisci. Impedit nisi praesentium, velit minima blanditiis assumenda obcaecati doloremque minus ea possimus?`
+
 const Entry = () => {
-	const imagenTest = `https://www.research4life.org/wp-content/uploads/2020/05/AdobeStock_162765779_2x1.jpg`
+
 	return (
 		<div className='Entry'>
 			<figure className='imagen'>
 				<img src={imagenTest} alt="" />
 			</figure>
-			<div className='entry-content'></div>
+			<div className='entry-content'>
+				<h1 className='entry-titles'>Epic Title</h1>
+				<p>{loremTest}</p>
+			</div>
 		</div>
 	)
 }
