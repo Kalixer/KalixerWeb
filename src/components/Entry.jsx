e55b7924792fde22e3baa2b94e7c79487940c21c
kalixer <al.caceres.ag72@gmail.com>
2023-07-13 12:40:14 -0400

    [Add] a 'handleClick' function added to go to the 'BlogTest' prototype when you click on an 'Entry'. And a route added on the 'App.jsx' file to the prototype

--- src/components/Entry.jsx
+++ src/components/Entry.jsx
@@ -6,10 +6,14 @@ import '@styles/Entry.scss';
 const imagenTest = `https://www.research4life.org/wp-content/uploads/2020/05/AdobeStock_162765779_2x1.jpg`
 const loremTest = `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae exercitationem ipsa illo vero hic reiciendis nemo eaque adipisci. Impedit nisi praesentium, velit minima blanditiis assumenda obcaecati doloremque minus ea possimus?`
 
+const handleClick = () => {
+	window.location.href = '/blog-test'
+}
+
 const Entry = () => {
 
 	return (
-		<div className='Entry'>
+		<div className='Entry' onClick={handleClick}>
 			<figure className='imagen'>
 				<img src={imagenTest} alt="" />
 			</figure>
--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -3,16 +3,26 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from '@containers/Layout';
 import Home from "@components/Home";
 import NotFound from "@components/NotFound";
+import AboutPage from "@pages/AboutPage";
+import BlogPage from "@pages/BlogPage";
+import BooksPage from "@pages/BooksPage";
+import PortfolioPage from "@pages/PortfolioPage";
+
+import BlogTest from "@components/BlogTest";
 
 import '@styles/global.css'
 
 const App = () => {
     return (
-        // <BrowserRouter>
-        <BrowserRouter basename="/Kalixer/">
+        // <BrowserRouter basename="/Kalixer/">
+        <BrowserRouter>
             <Layout>
                 <Routes>
                     <Route exact path="/" element={<Home />} />
+                    <Route exact path="/about" element={<AboutPage />} />
+                    <Route exact path="/blog" element={<BlogPage/>} />
+                    <Route exact path="/books" element={<BooksPage/>} />
+                    <Route exact path="/portfolio" element={<PortfolioPage/>} />
 					<Route path="*" element={<NotFound/>} />
                 </Routes>
             </Layout>
