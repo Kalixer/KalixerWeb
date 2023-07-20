653f5aa841ee4cfd7b5cacb3497fc520da1ba9f2
kalixer <al.caceres.ag72@gmail.com>
2023-07-20 12:09:04 -0400

    [Add] I added more routes to the App.jsx

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -7,8 +7,14 @@ import AboutPage from "@pages/AboutPage";
 import BlogPage from "@pages/BlogPage";
 import BooksPage from "@pages/BooksPage";
 import PortfolioPage from "@pages/PortfolioPage";
+import Blog1 from "@blogs/Blog1";
+import Blog2 from "@blogs/Blog2";
+import Blog3 from "@blogs/Blog3";
+import Blog4 from "@blogs/Blog4";
+import Blog5 from "@blogs/Blog5";
+import Blog6 from "@blogs/Blog6";
 
-import BlogTest from "@components/BlogTest";
+import BlogTest from "@pages/BlogTest";
 
 import '@styles/global.css'
 
@@ -21,7 +27,17 @@ const App = () => {
                     <Route exact path="/" element={<Home />} />
                     <Route exact path="/about" element={<AboutPage />} />
                     <Route exact path="/blog" element={<BlogPage/>} />
-                    {/* <Route exact path="/blog-test" element={<BlogTest/>} /> */}
+                    <Route exact path="/blog-test" element={<BlogTest/>} />
+
+                    {/* These are the blogs */}
+                    <Route exact path="/blog1" element={<Blog1 />} />
+                    <Route exact path="/blog2" element={<Blog2 />} />
+                    <Route exact path="/blog3" element={<Blog3 />} />
+                    <Route exact path="/blog4" element={<Blog4 />} />
+                    <Route exact path="/blog5" element={<Blog5 />} />
+                    <Route exact path="/blog6" element={<Blog6 />} />
+                    {/* These are the blogs */}
+
                     <Route exact path="/books" element={<BooksPage/>} />
                     <Route exact path="/portfolio" element={<PortfolioPage/>} />
 					<Route path="*" element={<NotFound/>} />
