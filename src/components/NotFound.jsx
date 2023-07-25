c71da55c9d5223cb3470b7ac15eb39aa10304688
kalixer <al.caceres.ag72@gmail.com>
2023-07-25 10:50:46 -0400

    [Add] Now there is a 404 page and it's respective style file

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -1,11 +1,22 @@
 import React from "react";
+import '@styles/NotFound.scss'
 
 
 const NotFound = () => {
+    const pink_read = 'https://i.pinimg.com/564x/28/70/af/2870af8d36ea1c178cdad089e46dc677.jpg'
     return (
-        <h1>404 Not found</h1>
-
-    );
+        <>
+            <div className='NotFound'>
+                <figure>
+                    <img src={pink_read} alt="" />
+                </figure>
+                <div className='message'>
+                    <h1>404 Error</h1>
+                    <h2>This page does not exist</h2>
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default NotFound;
