803c65ada935162350f12124160651df6201c3cd
kalixer <al.caceres.ag72@gmail.com>
2023-08-23 15:30:30 -0400

    [Mod] Implemented the 'scroll to top' function on the Entries

--- src/components/Entry.jsx
+++ src/components/Entry.jsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 
 import '@styles/Entry.scss';
 
+import scrollToTop from '@utils/scrollToTop';
 
 const loremTest = `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae exercitationem ipsa illo vero hic reiciendis nemo eaque adipisci. Impedit nisi praesentium, velit minima blanditiis assumenda obcaecati doloremque minus ea possimus?`
 
@@ -11,7 +12,7 @@ const Entry = (props) => {
 	
 	return (
 		<div className='Entry'>
-			<Link to={link}>
+			<Link to={link} onClick={scrollToTop}>
 				<figure className='imagen'>
 					<img src={props.image} alt="" />
 				</figure>
