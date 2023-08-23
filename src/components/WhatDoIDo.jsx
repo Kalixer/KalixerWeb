bf6cd2a441b5f4ea620da7653b31360720c1edb8
kalixer <al.caceres.ag72@gmail.com>
2023-08-23 09:34:06 -0400

    [Progress] The section 'WhatDoIDo' is ready in desktop, at least as a layout

--- src/components/WhatDoIDo.jsx
+++ src/components/WhatDoIDo.jsx
@@ -5,6 +5,11 @@ import '@styles/WhatDoIDo.scss'
 const message_gpt = 'An avid polymath with a passion for programming, avid reading, and artistics. '
 const macro_message = `"A buman being should be able to [do a lot of things]. Specialization is for insects." - Robert A. Heinlein`
 
+const studies_des = `I've been studing by myself Front-End development, Python and English`
+const exp_des = 'This website, Dice roller website, Kalixtore.us'
+const hobbies_des = 'Drawing, reading, film appreciation, videogames'
+const blog_des = 'A few entries to print on "paper" my thoughts'
+
 const WhatDoIDo = () => {
     return (
         <div className="What_do_i_do">
@@ -17,19 +22,19 @@ const WhatDoIDo = () => {
             <div className='macro_right'>
                 <div className="do_box">
                     <h1>Studies</h1>
-                    <p></p>
+                    <p>{studies_des}</p>
                 </div>
                 <div className="do_box">
                     <h1>Experience and skills</h1>
-                    <p></p>
+                    <p>{exp_des}</p>
                 </div>
                 <div className="do_box">
                     <h1>Hobbies</h1>
-                    <p></p>
+                    <p>{hobbies_des}</p>
                 </div>
                 <div className="do_box">
-                    <h1>Test</h1>
-                    <p></p>
+                    <h1>Blog</h1>
+                    <p>{blog_des}</p>
                 </div>
 
             </div>
