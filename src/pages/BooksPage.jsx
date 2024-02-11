f6fc1d8cffa6053646d61ded1b3c42e1200b8d49
kalixer <al.caceres.ag72@gmail.com>
2024-02-11 16:33:09 -0300

    [Add] I added a function that uses react state that filtes the category by clicking a button and then displays the items on screen

--- src/pages/BooksPage.jsx
+++ src/pages/BooksPage.jsx
@@ -3,46 +3,44 @@ import IntroSection from '@components/IntroSection';
 import '@styles/BooksPage.scss';
 import profile from '@assets/profile.jpeg'
 
-
-const category = [
-    { title: 'Films' },
-    { title: 'Series' },
-    { title: 'Books' },
-    { title: 'Videogames' },
-]
-
-const entries= [
+const allEntries= [
     { category: 'Films', title: 'Inception', imageSrc: '', imageTest: profile},
     { category: 'Films', title: 'The Dark Knight',  imageSrc: '', imageTest: profile},
     { category: 'Books', title: 'Never Split The Difference',  imageSrc: '', imageTest: profile},
     { category: 'Books', title: 'Quiet',  imageSrc: '', imageTest: profile},
     { category: 'Series', title: 'SmallVille',  imageSrc: '', imageTest: profile},
-    { category: 'Series', title: 'SmallVille',  imageSrc: '', imageTest: profile},
+    { category: 'Series', title: 'Clone Wars',  imageSrc: '', imageTest: profile},
     { category: 'VideoGames', title: 'Forza Motorsport',  imageSrc: '', imageTest: profile},
     { category: 'VideoGames', title: 'Forza Horizon 5',  imageSrc: '', imageTest: profile},
     { category: 'VideoGames', title: 'Outer Wilds',  imageSrc: '', imageTest: profile},
     { category: 'VideoGames', title: 'Escape Academy',  imageSrc: '', imageTest: profile},
 ]
-const filmsData = entries.filter(entry => entry.category === 'Films')
-const seriesData = {}
-const booksData = {}
-const videogamesData = {}
 
 const Books = () => {
+    const [entries, setEntries] = React.useState(allEntries)
+    
+
+    function selectCategory (categoria) {
+        const entriesFiltered = allEntries.filter(entries => entries.category === categoria)
+        setEntries(entriesFiltered)
+    }
     return (
         <div className='Books'>
             <IntroSection />
             <section className='content'>
                 <nav className='type-nav'>
-                    <div className='category'>Films</div>
-                    <div className='category'>Series</div>
-                    <div className='category'>Books</div>
-                    <div className='category'>Videogames</div>
+                    <div className='category' onClick={() => selectCategory('Films')}>Films</div>
+                    <div className='category' onClick={() => selectCategory('Series')}>Series</div>
+                    <div className='category' onClick={() => selectCategory('Books')}>Books</div>
+                    <div className='category' onClick={() => selectCategory('VideoGames')}>Videogames</div>
                 </nav>
                 <div className='books-content'>
                     {entries.map(
                         (entry) => (
-                            <div className='content-entry'>{entry.title}</div>
+                            <div 
+                            key={entry.title} 
+                            className='content-entry'>{entry.title}
+                            </div>
                         )
                     )}
                 </div>
