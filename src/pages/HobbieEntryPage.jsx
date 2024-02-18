0feacb7b824776050310adba403f3e057be50353
kalixer <al.caceres.ag72@gmail.com>
2024-02-18 12:39:45 -0300

    [Upgrade] The info block on the HobbieEntryPage is finished

--- src/pages/HobbieEntryPage.jsx
+++ src/pages/HobbieEntryPage.jsx
@@ -8,6 +8,7 @@ import '@styles/HobbieEntryPage.scss'
 const review = `Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque velit fugit, quis consectetur voluptatem ab ratione, earum autem deserunt eaque placeat deleniti mollitia ad quo fuga! Rerum asperiores ducimus dolore?
 Neque quasi voluptas culpa amet consequuntur beatae quis doloribus illo! Accusantium explicabo vero voluptate pariatur commodi rerum ut incidunt numquam itaque asperiores corporis similique maiores, illo natus possimus reprehenderit aperiam?`
 
+const quote = `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam, blanditiis nemo. Quia, dolore animi. Accusantium corrupti reprehenderit corporis cum voluptatem! Dolorem recusandae quae nobis veritatis pariatur rerum atque eaque molestias.`
 function HobbieEntryPage({hobbieEntry, introSection}) {
   return (
     <>
@@ -23,7 +24,18 @@ function HobbieEntryPage({hobbieEntry, introSection}) {
             <div className='review'>
                 <h2>An amazing introduction</h2>
                 <p>{review} <br /> {review}</p>
-                <div className='blockInfo'></div>
+                <div className='blockInfo'>
+                  <div className='block-image-container'>
+                    <figure>
+                      <img src={hobbieEntry.imagenBackground} alt="" />
+                    </figure>
+                  </div>
+                  <div className='block-quote'>
+                    <h3>Title of the quote</h3>
+                    <p>{quote}</p>
+                    <p>Writer</p>
+                  </div>
+                </div>
                 <p>{review} <br /> {review}</p>
             </div>
             <div className='backgroundStatic'></div>
