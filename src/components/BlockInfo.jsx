6fef650b8fe931d88e1fbaf8301c2f1f1459bb45
kalixer <al.caceres.ag72@gmail.com>
2024-02-18 12:46:03 -0300

    [Mod] I separated in a diferent component the BlockInfo

---
+++ src/components/BlockInfo.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import '@styles/BlockInfo.scss'
+
+function BlockInfo({hobbieEntry}) {
+    const quote = `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam, blanditiis nemo. Quia, dolore animi. Accusantium corrupti reprehenderit corporis cum voluptatem! Dolorem recusandae quae nobis veritatis pariatur rerum atque eaque molestias.`
+    
+    return (
+      <div className='BlockInfo'>
+        <div className='block-image-container'>
+          <figure>
+            <img src={hobbieEntry.imagenBackground} alt="" />
+          </figure>
+        </div>
+        <div className='block-quote'>
+          <h3>Title of the quote</h3>
+          <p>{quote}</p>
+          <p>Writer</p>
+        </div>
+      </div>
+    )
+}
+
+export { BlockInfo }
--- src/pages/HobbieEntryPage.jsx
+++ src/pages/HobbieEntryPage.jsx
@@ -4,12 +4,12 @@ import IntroSection from '@components/IntroSection'
 
 
 import '@styles/HobbieEntryPage.scss'
+import { BlockInfo } from '../components/BlockInfo'
 
-const review = `Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque velit fugit, quis consectetur voluptatem ab ratione, earum autem deserunt eaque placeat deleniti mollitia ad quo fuga! Rerum asperiores ducimus dolore?
-Neque quasi voluptas culpa amet consequuntur beatae quis doloribus illo! Accusantium explicabo vero voluptate pariatur commodi rerum ut incidunt numquam itaque asperiores corporis similique maiores, illo natus possimus reprehenderit aperiam?`
-
-const quote = `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam, blanditiis nemo. Quia, dolore animi. Accusantium corrupti reprehenderit corporis cum voluptatem! Dolorem recusandae quae nobis veritatis pariatur rerum atque eaque molestias.`
 function HobbieEntryPage({hobbieEntry, introSection}) {
+  const review = `Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque velit fugit, quis consectetur voluptatem ab ratione, earum autem deserunt eaque placeat deleniti mollitia ad quo fuga! Rerum asperiores ducimus dolore?
+  Neque quasi voluptas culpa amet consequuntur beatae quis doloribus illo! Accusantium explicabo vero voluptate pariatur commodi rerum ut incidunt numquam itaque asperiores corporis similique maiores, illo natus possimus reprehenderit aperiam?`
+
   return (
     <>
         <div className='HobbieEntryPage'>
@@ -24,18 +24,7 @@ function HobbieEntryPage({hobbieEntry, introSection}) {
             <div className='review'>
                 <h2>An amazing introduction</h2>
                 <p>{review} <br /> {review}</p>
-                <div className='blockInfo'>
-                  <div className='block-image-container'>
-                    <figure>
-                      <img src={hobbieEntry.imagenBackground} alt="" />
-                    </figure>
-                  </div>
-                  <div className='block-quote'>
-                    <h3>Title of the quote</h3>
-                    <p>{quote}</p>
-                    <p>Writer</p>
-                  </div>
-                </div>
+                <BlockInfo hobbieEntry={hobbieEntry}/>
                 <p>{review} <br /> {review}</p>
             </div>
             <div className='backgroundStatic'></div>
