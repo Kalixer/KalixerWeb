53ba9125a8400e992a5d5efb0ca9d3afb3dfccb4
kalixer <al.caceres.ag72@gmail.com>
2024-02-20 15:07:40 -0300

    [Mod] I added the property Link from react-router-dom to each of the pages

--- src/components/AboutBoxes.jsx
+++ src/components/AboutBoxes.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
+import scrollToTop from '../utils/scrollToTop'
 import '@styles/AboutBoxes.scss';
-
-import blogData from '@utils/blogData'
 
 const study_img = 'https://images.pexels.com/photos/577585/pexels-photo-577585.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
 const exp_img = 'https://images.pexels.com/photos/109371/pexels-photo-109371.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
@@ -10,32 +9,31 @@ const hobbie_img = 'https://images.pexels.com/photos/117018/pexels-photo-117018.
 const blog_img = 'https://images.pexels.com/photos/261579/pexels-photo-261579.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
 
 const aboutBoxesArr = [
-    {subject: 'studies', image: study_img, title: 'Studies', description: `For quite a time I've been studing Web development, English and I some of Python`},
-    {subject: 'experiences', image: exp_img, title: 'Experiences', description: `I have been testing my knowledge and building a few projects like: This website, Dice-Roller and Kalixtore.us`},
-    {subject: 'hobbies', image: hobbie_img, title: 'Hobbies', description: `Film appreciation, drawing, reading and playing some videogames are the things that I choose to do to relax and develop other areas of my interests`},
-    {subject: 'blog', image: blog_img, title: 'Blog', description: `Self-Understanding is quite a challenge, so I try sometimes to write down what I think to decrease my anxiety and record my thoughts`},
+    {subject: 'studies', image: study_img, link: 'https://platzi.com/p/Kalixer72/', title: 'Studies', description: `For quite a time I've been studing Web development, English and I some of Python`},
+    {subject: 'experiences', image: exp_img, link: '/portfolio', title: 'Experiences', description: `I have been testing my knowledge and building a few projects like: This website, Dice-Roller and Kalixtore.us`},
+    {subject: 'hobbies', image: hobbie_img, link: '/hobbies', title: 'Hobbies', description: `Film appreciation, drawing, reading and playing some videogames are the things that I choose to do to relax and develop other areas of my interests`},
+    {subject: 'blog', image: blog_img, link: '/blog', title: 'Blog', description: `Self-Understanding is quite a challenge, so I try sometimes to write down what I think to decrease my anxiety and record my thoughts`},
 ]
 
-const AboutBoxes = () => {
-    const handleClick = () => {
-        console.log('working')
-    }
+const AboutBoxes = (link) => {
     return (
         <div className='AboutBoxes'>
 
             {(aboutBoxesArr.map((box) => (
-                <div className='box' onClick={handleClick}>
-                    <figure>
-                        <img src={box.image} alt={box.title} />
-                    </figure>
-                    <div className='text'>
-                        <h3>{box.title}</h3>
-                        <p>{box.description}</p>
+                <Link to={box.link} onClick={scrollToTop}>
+                    <div className='box' onClick={() => handleClick(box.link)}>
+                        <figure>
+                            <img src={box.image} alt={box.title} />
+                        </figure>
+                        <div className='text'>
+                            <h3>{box.title}</h3>
+                            <p>{box.description}</p>
+                        </div>
                     </div>
-                </div>
+                </Link>
                 ))
             )}
-            
+
         </div>
     )
 }
